3abb171e356199a6dbd6bd73d49a1d4ae1f12942
ItzRya4Life <144141096+ItzRya4Life@users.noreply.github.com>
2023-12-08 15:40:58 +0000

    Completed all tasks.

--- events.js
+++ events.js
@@ -19,7 +19,10 @@ ________.addEventListener("click", function() {
 });
 
 */
-
+jumpButton.addEventListener("click", function() {
+    mario.style.bottom = "120px" ;
+    coin.style.display = "none" ;
+});
 
 /* 2. When jump down button is clicked, Mario jumps down. 
 
@@ -28,7 +31,9 @@ ________.addEventListener("click", function() {
 });
 
 */
-
+jumpDownButton.addEventListener("click", function(){
+    mario.style="bottom: 35px";
+});
 
 /* 3. When change color button is clicked, background color changes. 
 
@@ -37,7 +42,9 @@ ________.addEventListener("click", function() {
 });
 
 */
-
+changeColorButton.addEventListener("click", function(){
+    background.style.backgroundColor = "skyblue";
+});
 
 /* 4. When end game button is clicked, the title of the page says game over. 
 
@@ -46,7 +53,9 @@ ________._______ ("___", ________() {
 });
 
 */
-
+endGameButton.addEventListener("click", function(){
+    title.innerHTML = "Game over.";
+});
 
 /* 5. When update score button is clicked, the counter score becomes "1" instead of 0. 
 
@@ -55,7 +64,9 @@ ________._______ ("___", ________() {
 });
 
 */
-
+updateScoreButton.addEventListener("click", function(){
+    score.innerHTML = 1;
+});
 
 /* 6: When clicking the "Grow" button the Mario image gets bigger. Write the whole click handler! 
 
@@ -64,3 +75,6 @@ ________._______ ("___", ________() {
 }
 
 */
+growButton.addEventListener("click",function(){
+    mario.style.width = "200px";
+});
